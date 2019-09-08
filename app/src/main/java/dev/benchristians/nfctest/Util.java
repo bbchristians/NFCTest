55ff365ea19b83563213f7094f263ce509fd0a32
bist333 <sec7185@rit.edu>
2019-09-07 23:57:10 -0400

    have a random time

--- app/src/main/java/dev/benchristians/nfctest/Util.java
+++ app/src/main/java/dev/benchristians/nfctest/Util.java
@@ -1,11 +1,25 @@
 package dev.benchristians.nfctest;
 
+import android.util.Log;
+
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Util {
 
     public static long getChildbirthTime() {
         Random random = new java.util.Random();
-        return System.currentTimeMillis() + random.nextInt(30000) + 15000;
+        long eventTime = System.currentTimeMillis() + random.nextInt(30000) + 15000;
+        new Timer().schedule(new TimedEvent(), eventTime);
+        return eventTime;
+    }
+
+    public static class TimedEvent extends TimerTask {
+
+        @Override
+        public void run() {
+            Log.i("tag?","This is when the mating happens");
+        }
     }
 }
